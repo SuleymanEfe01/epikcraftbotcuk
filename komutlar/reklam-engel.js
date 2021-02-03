c0cde73bb89248f19291e8f89b0064e90b0c172a
Glitch (dust-holy-cafe) <none>
2021-02-03 03:45:26 +0000

    😅🚔 Checkpoint ./komutlar/reklam-engel.js:28001568/3405

--- komutlar/reklam-engel.js
+++ komutlar/reklam-engel.js
@@ -1,39 +1,33 @@
-const Discord = require('discord.js')
 const db = require('quick.db')
-
-module.exports.go = async(client,message,args) => {
-    if (!args[0]){
-        const arg = new Discord.MessageEmbed()
-        .setDescription(`Reklam Engel için Doğru Kullanım: w?reklamengel aç / w?reklamengel kapat`)
-        .setColor("RED")
-        message.channel.send(arg)
-    }
-    if (args[0] === "aç"){
-        const ac = new Discord.MessageEmbed()
-        .setDescription(`Reklam Engel başarıyla açıldı! Artık reklamlar silinicek!`)
-        .setColor("RED")
-        message.channel.send(ac)
-      
-      db.set(`reklam_${message.guild.id}`, "acik")
-    }
-    if (args[0] === "kapat"){
-        const kapat = new Discord.MessageEmbed()
-        .setDescription(`Reklam Engel başarıyla kapatıldı! Artık reklamlar silinmeyecek!`)
-        .setColor("RED")
-        message.channel.send(kapat)
-      
-      db.set(`reklam_${message.guild.id}`, "kapali")
-    }
+const { Discord, MessageEmbed } = require('discord.js')
+ let ayarlar = require('../ayarlar.json')
+ var prefix = ayarlar.prefix
+exports.run = async (bot, message, args) => {
+  
+if (!args[0]) return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu çalıştırmak için \`aç\` veya \`kapat\` demen gerekiyor.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('`SUNUCUYU_YÖNET` yetkisine sahip olmalısın!')
+ 
+if (args[0] == 'aç') {
+if(db.has(`reklam_${message.guild.id}`)) return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komut zaten önceden \`açılmış\`. \n Kapatmak için: \`${prefix}reklam kapat\``).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+db.set(`reklam_${message.guild.id}`, 'acik')
+message.channel.send(new MessageEmbed().setDescription(`${message.author} Reklam filtersini başarıyla \`açtın\`.`).setColor('0x348f36').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+}
+if (args[0] == 'kapat') {
+if(!db.has(`reklam_${message.guild.id}`)) return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komut zaten önceden \`kapatılmış\`. \n Açmak için: \`${prefix}reklam aç\``).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+db.delete(`reklam_${message.guild.id}`)
+message.channel.send(new MessageEmbed().setDescription(`${message.author} Reklam filtersini başarıyla \`kapattın\`.`).setColor('0x348f36').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+}
 };
-
+ 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 0
-  }
-  exports.help = {
-    name: "reklamengel",
-    description: "Reklam engel açar yada kapatır.",
-    usage: "reklam-engel"
-  }
+  enabled: true,
+  guildOnly: false,
+  aliases: ['advertisement','reklam'],
+  permLevel: 0
+};
+ 
+exports.help = {
+  name: 'reklam-engelle',
+  description: '',
+  usage: ''
+};
